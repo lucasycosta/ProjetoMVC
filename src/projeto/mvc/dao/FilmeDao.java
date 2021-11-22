92cb8a676a1563c7db9563ac5062e9995f9362b4
Lucas Yamaguti <lucasycosta@gmail.com>
2021-11-22 17:50:00 -0300

    modificacao

--- src/projeto/mvc/dao/FilmeDao.java
+++ src/projeto/mvc/dao/FilmeDao.java
@@ -2,12 +2,16 @@ package projeto.mvc.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import projeto.mvc.model.Filme;
 
 public class FilmeDao {
 	
+	//METODO DE INCLUSAO
 	public void cadastrarFilme(Filme filme) throws Exception {
 		String sql = "INSERT INTO filme (nome, genero) values (?, ?)";
 		PreparedStatement pstm = null;
@@ -22,6 +26,68 @@ public class FilmeDao {
 			System.out.println("Filme salvo com sucesso!!!");
 			
 		} catch(SQLException e){
+			e.printStackTrace();
+		}
+	}
+	
+	//METODO DE CONSULTA
+	public List<Filme> getFilme(int id){
+		String sql = "SELECT * FROM filme WHERE id = ?";
+		List<Filme> filmes = new ArrayList<Filme>();
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		
+		try {
+			conn = ConexaoDao.createConnectionToPostgres();
+			pstm = conn.prepareStatement(sql);
+			pstm.setInt(1, id);
+			ResultSet rset = pstm.executeQuery();
+			
+			while(rset.next()) {
+				Filme filme = new Filme();
+				
+				filme.setId(rset.getInt("id"));
+				filme.setNome(rset.getString("nome"));
+				filme.setGenero(rset.getString("genero"));
+				
+				filmes.add(filme);
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return filmes;
+	}
+	
+	//METODO DE ATUALIZAÇÃO
+	public void atualizar(Filme filme) {
+		String sql = "UPDATE filme SET nome = ?, genero? WHERE id = ?";
+		Connection conn;
+		PreparedStatement pstm;
+		
+		try {
+			conn = ConexaoDao.createConnectionToPostgres();
+			pstm = conn.prepareStatement(sql);
+			pstm.setString(1, filme.getNome());
+			pstm.setString(2, filme.getGenero());
+			pstm.setInt(3, filme.getId());
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//METODO DE EXCLUSÃO
+	public void deletarFilme(int id) {
+		String sql = "DELETE FROM filme WHERE id = " + id;
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		
+		try {
+			conn = ConexaoDao.createConnectionToPostgres();
+			pstm = conn.prepareStatement(sql);
+			pstm.execute();
+			System.out.println("Filme deletado com sucesso!!!");
+			
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
--- src/projeto/mvc/view/FilmeView.java
+++ src/projeto/mvc/view/FilmeView.java
@@ -8,26 +8,73 @@ import projeto.mvc.model.Filme;
 public class FilmeView {
 	public static void main(String[] args) throws Exception {
 		Scanner input = new Scanner (System.in);
-		
-		System.out.print("O que você deseja fazer?");
-		System.out.println("\n |1 - incluir novo filme"+
-						   "\n |2 - consultar file existente"+
-						   "\n |3 - alterar os dados de um filme"+
-						   "\n |4 - deletar o cadastro de um filme");
-		int opcao = input.nextInt();
-		
-		switch(opcao) {
-		case 1: {			
-			Filme filme = new Filme();
+		char resp;
+		do {
+			System.out.print("O que você deseja fazer?");
+			System.out.println("\n |1 - incluir novo filme"+
+							   "\n |2 - consultar file existente"+
+							   "\n |3 - alterar os dados de um filme"+
+							   "\n |4 - deletar o cadastro de um filme");
+			int opcao = input.nextInt();
 			
-			System.out.print("Insira o nome do filme: ");
-			filme.setNome(input.next());
-			System.out.print("Insira o genero do filme: ");
-			filme.setGenero(input.next());
+			switch(opcao) {
+				case 1: {			
+					Filme filme = new Filme();
+				
+					System.out.print("Insira o nome do filme: ");
+					filme.setNome(input.next());
+					System.out.print("Insira o genero do filme: ");
+					filme.setGenero(input.next());
+					
+					FilmeDao filmeDao = new FilmeDao();
+					filmeDao.cadastrarFilme(filme);
+					
+					break;
+				}
+				case 2:{
+					System.out.print("Digite o id do filme: ");
+					int id = input.nextInt();
+					
+					FilmeDao filmeDao = new FilmeDao();
+					for(Filme f : filmeDao.getFilme(id)) {
+						System.out.println("ID: " + f.getId());
+						System.out.println("Nome: " + f.getNome());
+						System.out.println("Genero: " + f.getGenero());
+					}
+					break;
+				}
+				case 3:{
+					FilmeDao filmeDao = new FilmeDao();
+					System.out.print("Insira o id do filme: ");
+					int id = input.nextInt();
+
+					Filme f1 = new Filme();
+					f1.setId(id);
+					System.out.println("Insira o novo nome: ");
+					System.out.println("Insira o novo genero: ");
+					
+					filmeDao.atualizar(f1);
+					
+					break;
+				}
+				case 4: {
+					FilmeDao filmeDao = new FilmeDao();
+					System.out.println("Insira o id do filme que sera excluido");
+					int id = input.nextInt();
+					filmeDao.deletarFilme(id);
+					
+					break;
+				}
+			}
 			
-			FilmeDao filmeDao = new FilmeDao();
-			filmeDao.cadastrarFilme(filme);
+			System.out.println("Deseja continuar? <s> para sim e <n> para não");
+			resp = input.next().charAt(0);
+			
+		} while(resp != 'n');
+		
+		if(resp == 'n') {
+			System.out.println("Fim da operação!!!");
 		}
-		}
+		input.close();
 	}
 }
